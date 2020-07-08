224f2c51dc413ee71b7c5bc645832b85358b0684
Tyler Boykin <tyler.boykin@bylight.com>
2020-07-08 02:03:05 +0000

    Bare basic RPC example in place

--- src/main.cr
+++ src/main.cr
@@ -6,12 +6,16 @@ require "./mt_rpc.cr"
 #MtAgentGen.agent_create("myagent","127.0.0.1",9999,"myagent_cookie")
 #MtAgentGen.create_cookie_file("myagent_cookie","127.0.0.1")
 
-rpcserver = MtRpcServ.new
 
 procs = Process.fork do		
-	p "[*] Starting agent check-in"
-	MtMaster.run_server(MtMaster.init_server("localhost",9999))
+	#p "[*] Starting agent check-in"
+	#MtMaster.run_server(MtMaster.init_server("localhost",9999))
 	
 	p "[*] Starting RPC..."
-	rpcserver.rpc_init("localhost",9998)
+	MtRpc::Server.new("127.0.0.1",9998).run
 end
+
+sleep(2)
+
+client = MtRpc::Client.new("127.0.0.1",9998)
+client.rpc_test
--- src/mt_rpc.cr
+++ src/mt_rpc.cr
@@ -1,48 +1,22 @@
 require "simple_rpc"
+require "socket"
 
-
-class MtRpcServ
-
-	include SimpleRpc::Proto
-
-	def rpc_init(addr : String, port : Int32) : (Nil)
-		while true
-			procs = Process.fork do
-				MtRpcServ::Server.new(addr,port).run
-			end
-	
-		procs.wait
-		end
-
-		return
-	end
-end
-
-
-class MtRpcMethods
+class MtRpc
 
 	include SimpleRpc::Proto
 
-	def test() : String
-		return "OK"
+	def ok_test : String
+		connfd = TCPSocket.new("127.0.0.1", 12345)
+		connfd << "TEST from RPC\n"
+		connfd.close
+		return "OK\r\n"	
 	end
 
-end
-
-
-class MtRpcHooks < MtRpcMethods
-
-	include SimpleRpc::Proto
-
-	def rpc_test() : (Nil|Bool)
-
-		procs = Process.fork do
-			client = MtRpcHooks::Client.new("127.0.0.1",9998)
-			result = MtRpcHooks.test()
+	class Client
+		def rpc_test : (Nil|Bool)
+			result = ok_test()
 			p result
+			return
 		end
-
-		procs.wait
-		return
 	end
 end
